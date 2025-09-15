2440a5d9b73b7150a18574d77f145e0ed91ed9c5
Ryouta <ryomomo1203@gmail.com>
2025-09-15 17:53:10 +0900

    バーセルのanalytics追加

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import App from "./App.tsx";
 import { MantineProvider } from "@mantine/core";
 import { theme } from "./theme";
 import { ThemeProvider } from "./store/themeStore";
+import { Analytics } from "@vercel/analytics/next";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <Analytics />
     <MantineProvider theme={theme}>
       <ThemeProvider>
         <App />
